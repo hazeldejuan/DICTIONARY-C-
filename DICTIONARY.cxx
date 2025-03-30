1838821abab00681f21349e1609e45207739659f
hazeldejuan <hazeldejuan217@gmail.com>
2025-03-30 20:25:56 +0800

    Add files via upload

---
+++ DICTIONARY.cxx
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string a;
+	
+	cout <<"ENTER YOUR WORD:";
+	cin >>a;
+	
+	if (a == "ABACUS"){
+		cout << "FIRST CONPUTER";
+	}else if (a == "NAPIERS BONES"){
+		cout << "HAS 9 DIFFERENT IVORY STRIPS";
+	}else if (a == "PASCALINE"){
+		cout << "ALSO CALLED AN ARITHMITIC MACHINE OR ADDING MACHINE";
+	}else if (a == "STEPPED RECKONER"){
+		cout << "USED FLUTTED DRUMS INSTEAD OF GEARS";
+	}else if (a == "ANALYTICAL ENGINE"){
+		cout << "CAPABLE OF PERFORMINF OR SOLVIBG ANY MATHEMATICAL PROBLEM AND INFORMATION PERMANENTLY";
+	}else if (a == "TABULATING MACHINE"){
+		cout << "CAPABLE FOR TABULATING STATISTICS";
+	}else if (a == "DIFFERENTIAL ANALYZER"){
+		cout << "FIRST ELECTRONUC COMPUTER";
+	}else if (a == "MARK 1"){
+		cout << "CAPABLE OF PERFORMING LARGE CALCULATUON";
+	}else if (a == "FIRST GENERATION COMPUTER"){
+		cout << "THIS MACHINE  ARE SLOW HUGE AND EXPENSIVE";
+	}else if (a == "SECOND GENERATION COMOUTER"){
+		cout << "IT USES TRANSISTOR COMPUTERS";
+		}else{
+        cout<<"No word is found";
+    }
+    
+   return 0;
+}
+	
+	
